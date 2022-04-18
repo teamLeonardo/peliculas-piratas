dc951f24f59395cec2311eb10565830b8d4e4fd2
leonardosm314 <leonardosm3.14@gmail.com>
2022-04-17 22:52:11 -0500

    fix: correcion

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,4 +1,4 @@
-import { Card, CardActionArea, CardMedia } from "@mui/material"
+import { Card, CardActionArea, CardContent, CardMedia, Tooltip, Typography } from "@mui/material"
 import { useRouter } from "next/router"
 
 export const CardParrilla = (movie) => {
@@ -6,18 +6,41 @@ export const CardParrilla = (movie) => {
     if (!movie.poster_path) {
         return <></>
     }
-    return <Card sx={{ maxWidth: "auto", height: "auto" }} >
-        <CardActionArea>
-            <CardMedia
-                component="img"
-                height="350"
-                image={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-                alt={movie.title}
+    return <Tooltip title={movie.overview} placement="right-start">
+        <Card sx={{ maxWidth: "auto", height: "auto" }} >
+            <CardActionArea
+                style={{
+                    width: "100%",
+                    height: "100%"
+                }}
                 onClick={(e) => {
                     e.preventDefault()
                     push("/" + movie.id)
                 }}
-            />
-        </CardActionArea>
-    </Card>
+            >
+                <CardMedia
+                    component="img"
+                    image={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                    alt={movie.title}
+                    sx={{
+                        objectFit: " cover",
+                        height: "100%",
+                    }}
+                />
+                <CardContent sx={{
+                    margin: "0", padding: "5px 8px 0px",
+                    position: "absolute",
+                    left: "0",
+                    width: "100%",
+                    height: "3rem",
+                    bottom: "0",
+                    bgcolor: '#00000050'
+                }}>
+                    <Typography variant="body2" textAlign={"center"} color="text.secondary">
+                        {movie.title}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    </Tooltip>
 }
--- pages/[id].js
+++ pages/[id].js
@@ -99,6 +99,7 @@ export default function MoviePeli({ detail }) {
                         }}>
                             <iframe
                                 id="iframe"
+                                allowFullScreen
                                 src={"https://www.2embed.ru/embed/tmdb/movie?id=" + data.id}
                                 width="100%"
                                 height="100%"
